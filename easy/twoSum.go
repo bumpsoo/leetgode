7f8d27a7b86da27867a23b0ac23f5ba22df4ae0e
bumpsoo <bumpsoo063@protonmail.ch>
2023-11-13 14:53:36 +0000

    added twoSum

--- easy/twoSum.go
+++ easy/twoSum.go
@@ -1,13 +1,15 @@
 package easy
 
+
 func twoSum(nums []int, target int) []int {
 	l := len(nums)
 	for i := 0; i < l; i++ {
-		for j := i + 1; j< l; j++ {
-			if nums[i] + nums[j] == target {
+		for j := i + 1; j < l; j++ {
+			if nums[i]+nums[j] == target {
 				return []int{i, j}
 			}
 		}
 	}
 	return []int{}
 }
+
