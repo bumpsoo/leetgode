7eb42ced079d04696a1349a2e0c9ad81f49a8cda
bumpsoo <bumpsoo063@protonmail.ch>
2024-01-31 23:21:06 +0900

    gofmt

--- medium/evaluateReversePolishNotation.go
+++ medium/evaluateReversePolishNotation.go
@@ -6,41 +6,41 @@ import "strconv"
 type stack[T any] []T
 
 func (s *stack[T]) Pop() T {
-  l := len(*s)
-  ret := (*s)[l-1]
-  *s = (*s)[:l-1]
-  return ret
+	l := len(*s)
+	ret := (*s)[l-1]
+	*s = (*s)[:l-1]
+	return ret
 }
 
 func (s *stack[T]) Push(val T) {
-  *s = append(*s, val)
+	*s = append(*s, val)
 }
 
-func eval(s string) func(int,int) int {
-  switch s {
-    case "+":
-      return  func(left, right int) int {return left + right}
-    case "-":
-      return func(left, right int) int {return left - right}
-    case "*":
-      return func(left, right int) int {return left * right}
-    case "/":
-      return func(left, right int) int {return left / right}
-    }
-    return nil
+func eval(s string) func(int, int) int {
+	switch s {
+	case "+":
+		return func(left, right int) int { return left + right }
+	case "-":
+		return func(left, right int) int { return left - right }
+	case "*":
+		return func(left, right int) int { return left * right }
+	case "/":
+		return func(left, right int) int { return left / right }
+	}
+	return nil
 }
 
 func evalRPN(tokens []string) int {
-  s := stack[int]{}
-  for _, token := range tokens {
-    f := eval(token)
-    if f == nil {
-      t, _ := strconv.Atoi(token)
-      s.Push(t)
-    } else {
-      right, left := s.Pop(), s.Pop()
-      s.Push(f(left, right))
-    }
-  }
-  return s.Pop()
+	s := stack[int]{}
+	for _, token := range tokens {
+		f := eval(token)
+		if f == nil {
+			t, _ := strconv.Atoi(token)
+			s.Push(t)
+		} else {
+			right, left := s.Pop(), s.Pop()
+			s.Push(f(left, right))
+		}
+	}
+	return s.Pop()
 }
