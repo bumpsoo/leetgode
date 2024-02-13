3762f1db2c19294bbbc30b030e441369e6dfdd7a
bumpsoo <bumpsoo063@protonmail.ch>
2024-02-13 13:49:42 +0900

    https://leetcode.com/problems/find-first-palindromic-string-in-the-array

---
+++ easy/findFirstPalindromicStringInTheArray.go
@@ -0,0 +1,29 @@
+package easy
+
+// https://leetcode.com/problems/find-first-palindromic-string-in-the-array
+func firstPalindromeHelp(word string) bool {
+  length := len(word)
+  odd := length % 2
+  half := length / 2
+  s := make([]byte, 0, half)
+  for i := 0; i < half; i++ {
+    s = append(s, word[i])
+  }
+  for i := half + odd; i < length; i++ {
+    v := s[len(s)-1]
+    s = s[:len(s)-1]
+    if v != word[i] {
+      return false
+    }
+  }
+  return true
+}
+
+func firstPalindrome(words []string) string {
+  for _, word := range words {
+    if firstPalindromeHelp(word) {
+      return word
+    }
+  }
+  return ""
+}
---
+++ easy/findFirstPalindromicStringInTheArray_test.go
@@ -0,0 +1,16 @@
+package easy
+
+import "testing"
+
+func TestFirstPalindrome(t *testing.T) {
+  tc := map[string][]string{
+    "ada": {"abc","car","ada","racecar","cool"},
+    "racecar": {"notapalindrome","racecar"},
+    "": {"def","ghi"},
+  }
+  for val, arg := range tc {
+    if ret := firstPalindrome(arg); ret != val {
+      t.Fatal(ret)
+    }
+  }
+}
